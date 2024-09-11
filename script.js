004c237d3dccf1e23298acaf8cbe702ccc411d5c
tenniscoin <tenniscoin2024@gmail.com>
2024-09-11 13:33:13 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        alert('Messaggio inviato! Grazie per averci contattato.');
-    });
-});
